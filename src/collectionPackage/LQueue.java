60e59d65f1a476b3c8b8d868ea4f0e914096dec5
Pranjal Kanel <pranjaldeepkanel@gmail.com>
2021-08-29 14:23:14 +0545

    Queue functions

--- src/collectionPackage/LQueue.java
+++ src/collectionPackage/LQueue.java
@@ -1,5 +1,22 @@
 package collectionPackage;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 public class LQueue {
 
+	public static void main (String [] args) {
+		Queue<Integer> myQueue = new LinkedList<>();
+		
+		// offer returns false if it fails to insert
+		myQueue.offer(9);	
+		// add throws illegal state exception if failed to insert
+		myQueue.add(1);
+		myQueue.add(3);
+		myQueue.add(2);
+		
+		// get value in queue
+		myQueue.poll();
+	}
 }
