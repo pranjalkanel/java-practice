8f6b4f1cc76d31d9205fe129e8f98140520df831
Pranjal Kanel <pranjaldeepkanel@gmail.com>
2021-09-27 17:35:57 +0545

    Arragned Stream Functinos in StreamFunc2

--- src/streamsPac/StreamExercise.java
+++ src/streamsPac/StreamExercise.java
@@ -1,13 +1,23 @@
 package streamsPac;
 
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class StreamExercise {
 	
 	public static void main(String[] args) {
+		StreamExercise obj = new StreamExercise();
+		
+		//obj.streamFuncs1();
+		obj.streamFuncs2();
+	}
+	
+	public void streamFuncs1() {
 		List<PersonData> people = getPeople();
 		
 		// looping through collection forEach
@@ -44,6 +54,49 @@ public class StreamExercise {
 		result2.stream()
 			.max(Comparator.comparing(PersonData::getAge))
 			.ifPresent(p -> System.out.println(p.getName()));
+	}
+	
+	
+	// intermediate and terminal operations
+	public void streamFuncs2() {
+		
+		List<PersonData> people = getPeople();
+		
+		// Creating integer stream using IntStream class
+		IntStream.range(1, 10).forEach(System.out::println);
+		System.out.println("-----");
+		
+		// Skip function skips the number of elements specified -> here it skips the first 8 elements
+		IntStream.range(1, 10).skip(8).forEach(System.out::println);
+		
+		// Sum of an integer stream --> here sum is the terminal operation (reducer)
+		System.out.println("Sum of the range is " +
+				IntStream.range(1, 10).sum()
+				);
+		
+		// Getting sum of all the ages using Collector type
+		System.out.println("Sum of all the ages is " + 
+				people.stream()
+					.collect(Collectors.summingInt(PersonData::getAge))
+				);
+		
+		// Making Stream out of string using Stream.of --> usage of ascending sort
+		Stream.of("Pranjal","Udaya","Adarsh","Meru","Sodip")
+			.sorted()
+			.findFirst().ifPresent(System.out::println);
+		
+		// Filtering out string that equals to "Pranjal"
+		Stream.of("Pranjal","Udaya","Adarsh","Meru","Sodip")
+		.filter(name -> name.equalsIgnoreCase("pranjal"))
+		.forEach(System.out::println);
+		
+		
+		// Usage of map and average to find the average of the squared values
+		Arrays.stream(new int[] {2,4,6,8,10})
+		.map(x -> x * x)	// making changes to each value of the stream -- returns a new stream to work with
+		.average()			// returns an optional, calculates the average of provided stream
+		.ifPresent(System.out::println);
+		
 	}
 	
 	public static List<PersonData> getPeople(){
