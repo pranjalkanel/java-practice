e7c15c426cbf31c12365c355887bee62b332363a
Pranjal Kanel <pranjaldeepkanel@gmail.com>
2021-10-06 17:37:49 +0545

    check valid emails leetcode

--- src/handling/Shandling.java
+++ src/handling/Shandling.java
@@ -583,4 +583,19 @@ public class Shandling {
 		
 		Arrays.stream(result).forEach(System.out::print);
 	}
+	
+	//unique email address
+	public void validEmail() {
+		
+		String[] emails = {"test.email+alex@leetcode.com","test.e.mail+bob.cathy@leetcode.com","testemail+david@lee.tcode.com"};
+		
+		Set<String> normalized = new HashSet<>(); // used to save simplified email address, cost O(n) sapce.
+        for (String email : emails) {
+            String[] parts = email.split("@"); // split into local and domain parts.
+            String[] local = parts[0].split("\\+"); // split local by '+'.
+            normalized.add(local[0].replace(".", "") + "@" + parts[1]); // remove all '.', and concatenate '@' and domain.        
+        }
+        
+        Arrays.stream(emails).forEach(System.out::println);
+	}
 }
